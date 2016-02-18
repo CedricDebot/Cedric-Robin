edecf93a5c0709a468466d33c53734a652b470ca
Robin Lanneer <Robin Lanneer@172.18.186.250>
2016-02-18 12:16:08 +0100

    Afwerking layout beginscherm De border van listview is nog niet perfect De borders van de titels zijn ook niet perfect

---
+++ src/domein/Leerling.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domein;
+
+/**
+ *
+ * @author Robin Lanneer
+ */
+public class Leerling {
+    private String inschrijvingsNr;
+    private String famillieNaam;
+    private String voorNaam;
+    private String email;
+    //attribuut foto
+
+    public Leerling(String inschrijvingsNr, String famillieNaam, String voorNaam, String email) {
+        this.famillieNaam = famillieNaam;
+        this.voorNaam = voorNaam;
+        this.email = email;
+        this.inschrijvingsNr = inschrijvingsNr;
+    }
+
+    public String getInschrijvingsNr() {
+        return inschrijvingsNr;
+    }
+
+    public void setInschrijvingsNr(String inschrijvingsNr) {
+        this.inschrijvingsNr = inschrijvingsNr;
+    }
+
+    public String getFamillieNaam() {
+        return famillieNaam;
+    }
+
+    public void setFamillieNaam(String famillieNaam) {
+        this.famillieNaam = famillieNaam;
+    }
+
+    public String getVoorNaam() {
+        return voorNaam;
+    }
+
+    public void setVoorNaam(String voorNaam) {
+        this.voorNaam = voorNaam;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
