861f602243e09816ce9712c4ba1a4b53cc5fd6eb
Robin <Robin@192.168.0.105>
2016-03-05 18:01:40 +0100

    #18	AttitudeScherm Menu nog niet geimplementeerd Knop voegtoe moet nog in de listview komen

--- src/gui/Attitude.java
+++ src/gui/Attitude.java
@@ -9,13 +9,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 
@@ -28,7 +31,7 @@ public class Attitude extends GridPane{
     public Attitude() {
         
         //HoofdGrid
-         gridLinesVisibleProperty().set(true);
+         gridLinesVisibleProperty().set(false);
          ColumnConstraints col0 = new ColumnConstraints();
          col0.setPercentWidth(40);
          
@@ -48,7 +51,7 @@ public class Attitude extends GridPane{
          
          //OpmerkingPane
          GridPane OpmerkingenPane = new GridPane();
-         OpmerkingenPane.gridLinesVisibleProperty().set(true);
+         OpmerkingenPane.gridLinesVisibleProperty().set(false);
          ColumnConstraints col0OpmerkingPane = new ColumnConstraints();
          col0OpmerkingPane.setPercentWidth(100);
          col0OpmerkingPane.setHalignment(HPos.CENTER);
@@ -65,7 +68,7 @@ public class Attitude extends GridPane{
          OpmerkingenPane.getColumnConstraints().add(col0OpmerkingPane);
          OpmerkingenPane.getRowConstraints().addAll(row0AttitudeLogo,row1Label,row2Text);
          
-         Image attitude = new Image("images/character.png");
+         Image attitude = new Image("images/attitude-placeholder.png");
          ImageView attitudeView = new ImageView(attitude);
          attitudeView.setId("AttitudeImage");
          OpmerkingenPane.add(attitudeView,0 , 0);
@@ -80,6 +83,7 @@ public class Attitude extends GridPane{
          
          add(OpmerkingenPane,0,0);
          
+         //listview
          ListView opmerkingenList = new ListView();
          opmerkingenList.setId("OpmeringenList");
          ObservableList<String> standaarOpmerkingen = 
@@ -88,17 +92,23 @@ public class Attitude extends GridPane{
                          "Ongeduldig", "Agressief rijgedrag", "Inzet", "Verstrooid",
                          "Eigenwijs");
          opmerkingenList.setItems(standaarOpmerkingen);
-         add(opmerkingenList, 1, 0);
          
-
+         Button voegToe = new Button("Voeg Toe");
+         TextField nieuw = new TextField();
+         HBox nieuwHB = new HBox();
+         nieuwHB.setId("nieuwHB");
+         nieuwHB.getChildren().addAll(nieuw, voegToe);
          
+         voegToe.setId("attitudeVoegToe");
+         VBox attitudeList = new VBox();
+         attitudeList.setId("attitudeList");
          
-
+         attitudeList.getChildren().addAll(opmerkingenList, nieuwHB);
+         add(attitudeList, 1, 0);
          
-         
-         
-
+         voegToe.setOnAction(e ->{
+             standaarOpmerkingen.add(nieuw.getText());
+             nieuw.clear();
+         });
     }
-    
-   
 }
